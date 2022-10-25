9669fd7b0cc52e7fcee6f4326b5e983d796c7629
nshark <noah@sitkoff.net>
2022-10-25 12:10:56 -0400

    Naming convention updated; readibilty enhanced.

--- src/com/company/ChompPlayer.java
+++ src/com/company/ChompPlayer.java
@@ -10,15 +10,15 @@ public class ChompPlayer {
         for (int i = 0; i < 10; i++) {
             c[i] = 0;
         }
-        states.put(process(c), new Point(0,0));
+        states.put(encode(c), new Point(0,0));
     }
     public Point AImove(int[] boardState){
         HashMap<Point, int[]> possibleMove =  possibleMoves(boardState);
         for(Point p : possibleMove.keySet()){
-            if (!states.containsKey(process(possibleMove.get(p)))){
-                states.put(process(possibleMove.get(p)), AImove(possibleMove.get(p)));
+            if (!states.containsKey(encode(possibleMove.get(p)))){
+                states.put(encode(possibleMove.get(p)), AImove(possibleMove.get(p)));
             }
-            if (states.get(process(possibleMove.get(p))) == null){
+            if (states.get(encode(possibleMove.get(p))) == null){
                 return p;
             }
         }
@@ -27,12 +27,8 @@ public class ChompPlayer {
     private int[] resolveMove(int[] boardState, int x, int y){
         int[] newBoardState = new int[10];
         System.arraycopy(boardState, 0, newBoardState, 0, 10);
-        boolean cut = false;
         for (int i = 0; i < 10; i++) {
-            if (x == i){
-                cut = true;
-            }
-            if (cut && newBoardState[i] > y) {
+            if (x <= i && newBoardState[i] > y) {
                 newBoardState[i] = y;
             }
         }
@@ -41,15 +37,13 @@ public class ChompPlayer {
     private HashMap<Point, int[]> possibleMoves(int[] boardState){
         HashMap<Point, int[]> list = new HashMap<>();
         for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                if (boardState[i] > j){
-                    list.put(new Point(i,j), resolveMove(boardState, i, j));
-                }
+            for (int j = 0; j < boardState[i]; j++) {
+                list.put(new Point(i,j), resolveMove(boardState, i, j));
             }
         }
         return list;
     }
-    public long process(int[] state){
+    public long encode(int[] state){
         long v = 0;
         for (int i = 0; i < 10; i++) {
             v += state[i] * Math.pow(11,i);
